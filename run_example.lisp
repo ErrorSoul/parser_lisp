aad2e7a515bf76cc66b6e3fdf8ff440b18503b7d
Abu Sultanov <neurofag@gmail.com>
2014-09-21 03:05:55 +0400

    working with macros

--- run_example.lisp
+++ run_example.lisp
@@ -47,10 +47,15 @@
 
 
 (defclass tag ()
-  ((name :accessor tag-name
-	 :initarg :name
+  ((name :accessor tag-tag
+	 :initarg :tag
 	 :initform "none")
-   (attribute :accessor tag-attribute
+
+   (content   :accessor tag-content
+	      :initarg :content
+	      :initform "")
+
+   (attribute :accessor tag-attr
 	      :initarg :attribute
 	      :initform (make-hash-table))
    (intags    :accessor tag-intags
@@ -79,19 +84,101 @@
     (range2 start end acc)))
 
 
+(defun addc (str chr)
+  (concatenate 'string str (list chr)))
 
-(defun string-split (str sep)
+(defun split (str sep)
   (let ((acc nil)
 	(flag 0)
 	(buf "")
-	(s (concatenate 'string str (list sep))))
+	(s (addc str sep)))
     (defun parser (chr)
       (cond ((equal chr sep) (progn 
 			       (setf acc (append acc (list buf)))
 			       (setf buf "")))
-	    (t (setf buf (concatenate 'string buf (list chr))))))
+	    (t (setf buf (addc buf chr)))))
     (map nil #'parser s)
-    (pprint acc)
-    (print "dfdsfadsf")))
+    acc))
 
 
+(defun string-split (str sep)
+  (cond ((string= "" str) ( print nil))
+	((= (length str) 1) (string (char str 0)))
+	(t (split str sep))))
+
+(defun change-tag ()
+  (when (not (null current-tag))
+    
+    (push current-tag list-of-tag))
+  (setf current-tag (make-instance 'tag)))
+
+(defun back-tag ()
+  (if (equal tagbuf (tag-name current-tag))
+      (when (not (null (car list-of-tag)))
+	(add (car list-of-tag) current-tag)
+	(setf current-tag (pop list-of-tag)))))
+  
+  
+  
+(defun analizator (chr)
+  (case chr
+    (#\< (setf tagflag 1)
+         (when-flag "content")
+	 (setf newtagflag 1))
+
+    (#\Space (when (= tagflag 1)
+	       (setf attrflag 1))
+             (when-flag tag))
+     
+    (#\/ (when (= newtagflag 1)
+	   (setf newtagflag 0)
+	   (setf backtagflag 1)))
+	   
+    (#\> (when-flag attr)
+         (setf contentflag 1)
+	 (when (= backtagflag 0)
+	   (when-flag "tag"))
+	 (when (= backtagflag 1)
+	   (setf backtagflag 0)
+	   (back-tag)))
+
+    (otherwise (cond
+		 ((= newtagflag 1) (setf newtagflag 0) (change-tag) (setf tagbuf (addc tagbuf chr)))
+		 ((= tagflag 1) (setf tagbuf (addc tagbuf chr)))
+		 ((= attrflag 1) (setf attrbuf (addc attrbuf chr)))
+		 ((= contentflag 1) (setf contentbuf (addc contentbuf chr)))))))
+
+
+(defun analiz-init (str)
+  (let ((tagflag 0)
+	(attrflag 0)
+	(newtagflag 0)
+	(taglist nil)
+	(tagbuf "")
+	(attrlist nil)
+	(attrbuf ""))
+	
+
+
+(defun when-tagflag ()
+  (when (= tagflag 1)
+       (setf tagflag 0)
+       (setf (tag-tag current-tag) tagbuf)
+       (setf tagbuf "")))
+
+(defun when-contentflag ()
+  (when (= contentflag 1)
+    (setf contentflag 0)
+    (setf x 1)))
+
+
+(defmacro when-flag (name)
+  (let ((flag (read-from-string (concatenate 'string (string name) "flag")))
+	(lst (read-from-string (concatenate 'string (string name) "list")))
+	(buf (read-from-string (concatenate 'string (string name) "buf"))))
+    `(when (= ,flag 1)
+       (setf ,flag 0)
+       (push ,buf ,lst)
+       (setf ,buf ""))))
+       
+       
